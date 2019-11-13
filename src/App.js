5626de0e7e747c4c57c25318dc737e07163194ac
Todd Trivisonno <todd.trivisonno@gmail.com>
2019-11-13 10:29:22 -0500

    axios api and display package in html

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class PersonList extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      array: []
+    }
+  }
+
+  // --- Get and Set API. Add API response to my JSON --- //
+  async getLaravelStuff() {
+    const axios = require('axios');
+    const userResponse = await axios.get('http://127.0.0.1:8000/api/users');
+    const profilesResponse = await axios.get('http://127.0.0.1:8000/api/profiles');
+    const followsResponse = await axios.get('http://127.0.0.1:8000/api/follows');
+    const activitiesResponse = await axios.get('http://127.0.0.1:8000/api/activities');
+    const kudosResponse = await axios.get('http://127.0.0.1:8000/api/kudos');
+
+    console.log(userResponse.data.data);
+    console.log(profilesResponse.data.data);
+    console.log(followsResponse.data.data);
+    console.log(activitiesResponse.data.data);
+    console.log(kudosResponse.data.data);
+
+    this.setState({
+      users: userResponse.data.data,
+      profiles: profilesResponse.data.data,
+      follows: followsResponse.data.data,
+      activities: activitiesResponse.data.data,
+      kudos: kudosResponse.data.data
+    })
+
+  }
+
+  componentDidMount() {
+    this.getLaravelStuff();
+  }
+
+  render() {
+
+    console.log(this.state.users);
+
+    return (
+      <p>
+        {this.state.users ? this.state.users[0].name : 'Loading'}
+      </p>
+    )
+  }
 }
 
-export default App;
+// export default App;
